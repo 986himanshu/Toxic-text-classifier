44c8184c9e8a1f5a91cf4f638b5661be523b2c52
Himanshu Sharma <986himanshusharma@gmail.com>
2024-11-27 12:00:57 +0530

    Created a project structure using automated python script

---
+++ template.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level= logging.INFO, format= '[%(asctime)s]: %(message)s:')
+
+project_name = "TextClassifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/init.py",
+    f"src/{project_name}/components/init.py",
+    f"src/{project_name}/utils/init.py",
+    f"src/{project_name}/config/init.py",
+    f"src/{project_name}/config/configuration/init.py",
+    f"src/{project_name}/pipeline/init.py",
+    f"src/{project_name}/entity/init.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/live_code.ipynb"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+    if (not os.path.exists(filepath) or (os.path.getsize(filepath)== 0)):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"Creating empty file: {filename}")
+    else:
+        logging.info(f"{filename} already exists")
